ad001cba4b26c0138f5b92f9037b981888ffd43f
Hetpatel62 <patelhet3349@gmail.com>
2026-02-28 13:32:20 -0600

    feat: implement form validation and error handling for listings

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -50,11 +51,11 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create Route
-app.post("/listings", async (req, res) => {
+app.post("/listings", wrapAsync(async (req, res,next) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
-});
+}));
 
 //Edit Route
 app.get("/listings/:id/edit", async (req, res) => {
@@ -76,6 +77,12 @@ app.delete("/listings/:id", async (req, res) => {
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
   res.redirect("/listings");
+});
+
+
+app.use((req,res,next,err)  => {
+  console.log(err);
+  res.status(500).send("Something went wrong!");
 });
 
  /*app.get("/testListing", async (req, res) => {
